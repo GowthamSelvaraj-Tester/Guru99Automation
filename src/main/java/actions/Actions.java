0dfcb230acebbb55aae81b9b8d40044af7889679
GowthamSelvaraj-Tester <gautamselvaraj003@gmail.com>
2024-05-29 21:14:07 +0530

    Commit for removing unused imports

--- src/main/java/actions/Actions.java
+++ src/main/java/actions/Actions.java
@@ -2,19 +2,15 @@ package actions;
 
 import java.time.Duration;
 import java.util.List;
-
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import utility.ConfigReader;
+
 
 public class Actions 
 	{
--- src/main/java/pageObject/RegistrationPageObject.java
+++ src/main/java/pageObject/RegistrationPageObject.java
@@ -6,9 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
-import utility.ConfigReader;
+
 
 public class RegistrationPageObject 
 	{
